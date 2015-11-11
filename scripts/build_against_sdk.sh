d4b60fde1af14b0dc1373f5f798b2db968db7934
Raul Ochoa <rochoaf@gmail.com>
2015-11-11 15:17:57 +0100

    Upgrade mapnik sdk to 691-ga22b3e5

--- scripts/build_against_sdk.sh
+++ scripts/build_against_sdk.sh
@@ -98,7 +98,7 @@ if [[ "${CXX11:-false}" != false ]]; then
     HASH="1702-g65bd9e6"
 else
     # mapnik 2.3.x / c++11 not enabled
-    HASH="688-g9d40bb2"
+    HASH="691-ga22b3e5"
 fi
 
 if [[ ${platform} == 'linux' ]]; then
